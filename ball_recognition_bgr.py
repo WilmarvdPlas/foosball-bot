d83607c8ac3aaaee2ac47b70433fc1896e6951a6
Wilmar <s1137070@student.hsleiden.nl>
2024-12-19 18:37:37 +0100

    Tune shooting behaviour

--- ball_recognition_bgr.py
+++ ball_recognition_bgr.py
@@ -5,6 +5,7 @@ import math
 import pigpio
 import sys
 import signal
+import threading
 
 CAPTURE_WIDTH = 640
 CAPTURE_HEIGHT = 360
@@ -33,21 +34,25 @@ TRANSLATE_DEFENSE_DIR_PIN = 27
 ROTATE_DEFENSE_STEP_PIN = 13
 ROTATE_DEFENSE_DIR_PIN = 17
 
+ROTATE_DEFENSE_HALL_PIN = 23
+
 PI.set_mode(TRANSLATE_DEFENSE_STEP_PIN, pigpio.OUTPUT)
 PI.set_mode(TRANSLATE_DEFENSE_DIR_PIN, pigpio.OUTPUT)
 PI.set_mode(ROTATE_DEFENSE_STEP_PIN, pigpio.OUTPUT)
 PI.set_mode(ROTATE_DEFENSE_DIR_PIN, pigpio.OUTPUT)
 
-TRANSLATION_STOP_PERCENTAGE = 0.05
+PI.set_mode(ROTATE_DEFENSE_HALL_PIN, pigpio.INPUT)
+
+TRANSLATION_STOP_PERCENTAGE = 0.035
 
 MIN_TRANSLATE_FREQUENCY = 250
-MAX_TRANSLATE_FREQUENCY = 1350
+MAX_TRANSLATE_FREQUENCY = 1300
 
 BALL_TIMEOUT_SECONDS = 1
 
-PID_KP = 200
+PID_KP = 300
 PID_KI = 0
-PID_KD = 10
+PID_KD = 2
 
 pid_last_error = 0
 pid_cumulative_error = 0
@@ -57,6 +62,10 @@ last_ball_sighting_seconds = 0
 
 frame_count = 0
 fps_start_time = time.time()
+
+lock = threading.Lock()
+
+defense_shooting_active = False
 
 def compute_translation_PID(ball_y, player_y):
     global previous_pid_time_seconds, pid_last_error, pid_cumulative_error
@@ -78,6 +87,7 @@ def compute_translation_PID(ball_y, player_y):
 
 def cleanup(sig, frame):
     PI.hardware_PWM(TRANSLATE_DEFENSE_STEP_PIN, 0, 0)
+    PI.hardware_PWM(ROTATE_DEFENSE_STEP_PIN, 0, 0)
     PI.stop()
 
     CAPTURE.release()
@@ -87,6 +97,39 @@ def cleanup(sig, frame):
 
 def clamp(n, minn, maxn):
     return max(min(maxn, n), minn)
+
+def check_rotate_stop_condition(hall_pin, step_pin, stop_state):
+    global defense_shooting_active
+    previous_state = PI.read(hall_pin)
+
+    while True:
+        current_state = PI.read(hall_pin)
+
+        if previous_state != stop_state and current_state == stop_state:
+            PI.hardware_PWM(step_pin, 0, 0)
+            with lock:
+                defense_shooting_active = False
+            return
+        
+        previous_state = current_state
+
+        time.sleep(0.01)
+
+def shoot_forwards(step_pin, dir_pin, hall_pin):
+    PI.hardware_PWM(step_pin, 1100, 500000)
+
+    PI.write(dir_pin, pigpio.HIGH)
+    time.sleep(0.05)
+    PI.write(dir_pin, pigpio.LOW)
+
+    threading.Thread(target=check_rotate_stop_condition, args=(hall_pin, step_pin, True)).start()
+    
+def shoot_backwards(step_pin, dir_pin, hall_pin):
+    PI.hardware_PWM(step_pin, 1100, 500000)
+
+    PI.write(dir_pin, pigpio.HIGH)
+    
+    threading.Thread(target=check_rotate_stop_condition, args=(hall_pin, step_pin, False)).start()
 
 def filter_mask_by_percentage(mask, percentages):
     filtered_mask = np.zeros_like(mask)
@@ -140,7 +183,7 @@ def get_player_centers(frame):
         if len(center) < 2:
             continue
 
-        if (center[1] < RESIZED_WIDTH / 2):
+        if (center[1] > RESIZED_WIDTH / 2):
             attack_centers.append(center)
         else:
             defense_centers.append(center)
@@ -148,7 +191,7 @@ def get_player_centers(frame):
     return attack_centers, defense_centers
 
 def control_motors(ball_center, attack_centers, defense_centers):
-    global last_ball_sighting_seconds
+    global last_ball_sighting_seconds, defense_shooting_active
 
     if ball_center is None:
         if time.time() - last_ball_sighting_seconds > BALL_TIMEOUT_SECONDS:
@@ -159,22 +202,23 @@ def control_motors(ball_center, attack_centers, defense_centers):
     last_ball_sighting_seconds = time.time()
 
     player_y_zone = RESIZED_HEIGHT / PLAYERS_IN_ROW
-    active_player_index = clamp(math.floor(ball_center[1] / player_y_zone), 0, len(attack_centers) - 1)
+    active_player_index = clamp(math.floor(ball_center[1] / player_y_zone), 0, len(defense_centers) - 1)
 
-    if len(attack_centers) == PLAYERS_IN_ROW:
-        sorted_defense_centers = sorted(attack_centers, key=lambda x: x[0])
+    if len(defense_centers) == PLAYERS_IN_ROW:
+        sorted_defense_centers = sorted(defense_centers, key=lambda x: x[0])
         defense_coords = sorted_defense_centers[active_player_index]
 
         if abs(ball_center[1] - defense_coords[0]) < TRANSLATION_STOP_PERCENTAGE * RESIZED_HEIGHT:
             PI.hardware_PWM(TRANSLATE_DEFENSE_STEP_PIN, 0, 0)
 
-            if abs(ball_center[0] - defense_coords[1]) < 0.1 * RESIZED_WIDTH:
-                PI.write(ROTATE_DEFENSE_DIR_PIN, pigpio.HIGH) if ball_center[0] < defense_coords[1] else PI.write(ROTATE_DEFENSE_DIR_PIN, pigpio.LOW)
-                PI.hardware_PWM(ROTATE_DEFENSE_STEP_PIN, 1000, 500000)
-                time.sleep(0.25)
-                PI.write(ROTATE_DEFENSE_DIR_PIN, pigpio.LOW) if ball_center[0] < defense_coords[1] else PI.write(ROTATE_DEFENSE_DIR_PIN, pigpio.HIGH)
-                time.sleep(0.25)
-                PI.hardware_PWM(ROTATE_DEFENSE_STEP_PIN, 0, 0)
+            if abs(ball_center[0] - defense_coords[1]) < 0.1 * RESIZED_WIDTH and not defense_shooting_active:
+                with lock:
+                    defense_shooting_active = True
+
+                if ball_center[0] < defense_coords[1]:
+                    threading.Thread(target=shoot_forwards, args=(ROTATE_DEFENSE_STEP_PIN, ROTATE_DEFENSE_DIR_PIN, ROTATE_DEFENSE_HALL_PIN)).start()
+                else:
+                    threading.Thread(target=shoot_backwards, args=(ROTATE_DEFENSE_STEP_PIN, ROTATE_DEFENSE_DIR_PIN, ROTATE_DEFENSE_HALL_PIN)).start()
 
             return
 
